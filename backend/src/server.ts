cbf3d1e57645f57dd621f4c983108d85a3e97f75
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-15 22:50:51 +0200

    created note model and added error handling middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,6 @@
-import "dotenv/config";
+import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
 
 const { PORT: port, MONGODB_URI: uri } = env;
 
