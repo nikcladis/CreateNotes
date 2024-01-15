ec7753e7537fa2f1102f5793f17985f0f58aa83b
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-15 16:32:59 +0200

    connected mongodb database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
+import "dotenv/config";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+const { PORT: port, MONGODB_URI: uri } = env;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}!`);
+    });
+  })
+  .catch(console.error);
