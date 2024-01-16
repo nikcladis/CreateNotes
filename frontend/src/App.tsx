3b05b4e3bd847874c6aaa72c39ba96fed37a8d7b
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-16 20:56:57 +0200

    added frontend with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <div className="grid place-items-center h-screen">
+        <Button size="lg" variant="outline">
+          Click
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
