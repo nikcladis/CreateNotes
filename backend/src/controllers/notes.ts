b3a3bb6cafea4657d99be714aa769a277df81496
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-16 16:09:45 +0200

    added update and delete note

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -17,7 +17,7 @@ export const getNoteById: RequestHandler = async (req, res, next) => {
 
   try {
     if (!mongoose.isValidObjectId(noteId)) {
-      throw createHttpError(404, "Invalid note id");
+      throw createHttpError(400, "Invalid note id");
     }
 
     const note = await NoteModel.findById(noteId).exec();
@@ -52,6 +52,72 @@ export const createNote: RequestHandler<
 
     const newNote = await NoteModel.create({ title, text });
     res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+
+interface UpdateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  unknown
+> = async (req, res, next) => {
+  const { noteId } = req.params;
+  const { title: newTitle, text: newText } = req.body;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    if (!newTitle) {
+      throw createHttpError(400, "Title is required");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
+
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const { noteId } = req.params;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
+
+    await NoteModel.deleteOne({ _id: noteId });
+
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
